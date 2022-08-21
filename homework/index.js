01626568e2f257db503edec1be548c0a654b37d8
VLSLV333 <v.sherstyk@gmail.com>
2022-08-21 12:24:34 +0000

    ready to commit to gitlab

--- homework/index.js
+++ homework/index.js
@@ -1,18 +1,14 @@
-// Your code goes here
 function isEquals (a, b){
 return a === b;
 }
-// console.log(isEquals(3, 3)) // => true
 function isBigger (a, b){
 return a > b;
 }
-// console.log(isBigger(5, -1)) // => true
 function storeNames (a, ...b){
     let answer = [];
     answer.push(a, ...b);
     return answer
 }
-// console.log(storeNames('Tommy Shelby', 'Ragnar Lodbrok', 'Tom Hardy',));
 function getDifference(a, b){
     let answer = a - b;
     if (answer > 0){
@@ -23,7 +19,6 @@ function getDifference(a, b){
         return 0;
     }
 }
-// console.log(getDifference(-5, 10))// => 2
 function negativeCount (arr){
     let keyArray = [];
     for (let key of arr){
@@ -33,7 +28,6 @@ function negativeCount (arr){
     }
     return keyArray.length;
 }
-// console.log(negativeCount([0, -3, 5, 7])) // => 1
 function letterCount (str, piece){
     let answer = [];
     let arrayOfAllLetters = str.split('');
@@ -44,7 +38,6 @@ function letterCount (str, piece){
     }
     return answer.length;
 }
-// console.log(letterCount("Marry", "r")) // => 2
 function countPoints(arr){
     let points = 0;
     let pointsForWinning = 3;
@@ -58,4 +51,3 @@ function countPoints(arr){
     }
     return points;
 }
-// console.log(countPoints(['100:90', '110:98', '100:100', '95:46', '54:90', '99:44', '90:90', '111:100'])) // => 17
